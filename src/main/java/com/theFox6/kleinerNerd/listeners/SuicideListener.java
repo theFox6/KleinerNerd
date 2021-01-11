19ab0ffd9da91091bce3a9bdeb34e6fdfc2e4c3f
theFox6 <the.fox6@gmx.de>
2021-01-11 19:38:57 +0100

    added more ways of killing self
    and a litle feature preview

--- src/main/java/com/theFox6/kleinerNerd/listeners/SuicideListener.java
+++ src/main/java/com/theFox6/kleinerNerd/listeners/SuicideListener.java
@@ -20,17 +20,21 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionRemoveEvent;
 import net.dv8tion.jda.api.hooks.SubscribeEvent;
 
 public class SuicideListener {
 	private Map<String,Instant> lastSuicide = new ConcurrentHashMap<>();
 	private final PatternPart kms;
+	//TODO: auch pattern
 	private static final List<String> howMany = Arrays.asList(
-			"wie oft habe ich mich schon umgebracht?","wie oft habe ich mich schon umgebracht",
-			"wie oft habe ich mich schon erhängt?","wie oft habe ich mich schon erhängt");
+			"wie oft habe ich mich schon umgebracht?","wie oft habe ich mich umgebracht?","wie oft habe ich mich schon umgebracht","wie oft habe ich mich umgebracht",
+			"wie oft habe ich mich schon erhängt?","wie oft habe ich mich erhängt?","wie oft habe ich mich schon erhängt","wie oft habe ich mich erhängt");
 	
 	public SuicideListener() {
 		OptionalPattern od = new OptionalPattern(new StringPattern("."));
+		//$laterne = [{"ich gehe ","ich geh "},"mit meiner Laterne",(".")]
 		PatternPart z = new PatternJunction("ziel ",
 				new StringPattern("ich gehe "),
 				new StringPattern("ich geh "),
@@ -45,17 +49,36 @@ public class SuicideListener {
 						new PatternSequence(new StringPattern("ich bring mich um"),od),
 						new PatternSequence(new StringPattern("ich bringe mich um"),od)
 					),
-				new PatternSequence(z, new StringPattern("erhängen"),od),
+				new PatternSequence(z, new StringPattern("mich erhängen"),od),
 				new PatternSequence(z, new StringPattern("sterben"),od),
 				new PatternSequence(z, new StringPattern("mein leben beenden"), od),
 				new PatternSequence(z, new StringPattern("nicht mehr leben"),od),
 				new PatternSequence(z, new StringPattern("mich ertränken"),od),
+				new PatternSequence(z, new StringPattern("mich erschießen"),od),
+				new PatternSequence(z, new StringPattern("mich vergiften"),od),
+				new PatternJunction("klippe",
+						new PatternSequence(z, new StringPattern("mich von ner klippe stürzen"),od),
+						new PatternSequence(z, new StringPattern("mich von einer klippe stürzen"),od)
+					),
+				new PatternJunction("bus",
+						new PatternSequence(z, new StringPattern("mich vom bus überfahren lassen"),od),
+						new PatternSequence(z, new StringPattern("mich von einem bus überfahren lassen"),od)
+					),
+				new PatternJunction("pulsadern",
+						new PatternSequence(z, new StringPattern("meine pulsadern aufschlitzen"),od),
+						new PatternSequence(z, new StringPattern("mir die pulsadern aufschlitzen"),od),
+						new PatternSequence(z, new StringPattern("mir meine pulsadern aufschlitzen"),od)
+					),
 				new PatternJunction("lebenAbgeschlossen",
 						new PatternSequence(haben,new StringPattern("mein leben abgeschlossen"),od),
 						new PatternSequence(haben,new StringPattern("mit meinem leben abgeschlossen"),od),
 						new PatternSequence(haben,new StringPattern("abgeschlossen mit meinem leben"),od)
 					),
-				new PatternSequence(new StringPattern("i want to die"),od)
+				new PatternSequence(new StringPattern("i want to die"),od),
+				new PatternJunction("suicide",
+						new PatternSequence(new StringPattern("im gonna suicide"),od),
+						new PatternSequence(new StringPattern("i'm gonna suicide"),od)
+					)
 			);
 		//just a smoke test
 		if (!kms.matches("ich hab mit meinem leben abgeschlossen")) {
@@ -69,7 +92,7 @@ public class SuicideListener {
 		String raw = msg.getContentRaw();
 		//TODO: total suicide count
 		if (raw.equals(KleinerNerd.prefix + "totaldeaths")) {
-			CounterStorage.getUserTotalCount("suicides");
+			msg.getChannel().sendMessage(Integer.toString(CounterStorage.getUserTotalCount("suicides"))).queue();
 		} else if (kms.matches(raw.toLowerCase())) {
 			User author = msg.getAuthor();
 			String authorId = author.getId();
@@ -101,4 +124,28 @@ public class SuicideListener {
 	public static void reactWithF(Message msg) {
 		msg.addReaction("U+1f1eb").queue();
 	}
+	
+	/* coming soon
+	// TODO: do not record your own F reacts
+
+	@SubscribeEvent
+	public void onReact(GuildMessageReactionAddEvent event) {
+		if (event.getReactionEmote().getEmoji().equals("U+1f1eb")) {
+			String userId = event.getUserId();
+			checkFReactAchievement(userId, CounterStorage.getUserCounter("f_reacts", userId).incrementAndGet());
+		}
+	}
+	
+	@SubscribeEvent
+	public void onReactRemove(GuildMessageReactionRemoveEvent event) {
+		if (event.getReactionEmote().getEmoji().equals("U+1f1eb")) {
+			CounterStorage.getUserCounter("f_reacts", event.getUserId()).getAndDecrement();
+		}
+	}
+
+	private void checkFReactAchievement(String userId, int fcount) {
+		// TODO achievements for 10, 20, 50, 100, 200, 1000
+		// should be added before release
+	}
+	*/
 }
