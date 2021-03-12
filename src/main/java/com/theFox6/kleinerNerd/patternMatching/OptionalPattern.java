7c784e3ca2f78e5625f21c425c6977eec9926712
theFox6 <the.fox6@gmx.de>
2021-03-12 12:19:44 +0100

    make OptionalPattern accept subpatterns
    why ever it worked when I tested it...

--- src/main/java/com/theFox6/kleinerNerd/patternMatching/OptionalPattern.java
+++ src/main/java/com/theFox6/kleinerNerd/patternMatching/OptionalPattern.java
@@ -2,7 +2,7 @@ package com.theFox6.kleinerNerd.patternMatching;
 
 import java.util.stream.IntStream;
 
-public class OptionalPattern implements PatternPart {
+public class OptionalPattern implements PatternPart, NestingPattern {
 	private PatternPart optionalPart;
 
 	public OptionalPattern() {
@@ -26,5 +26,10 @@ public class OptionalPattern implements PatternPart {
 	@Override
 	public String stringRepresentation() {
 		return "("+optionalPart.stringRepresentation()+")";
+	}
+
+	@Override
+	public void accept(PatternPart option) {
+		optionalPart = option;
 	}
 }
